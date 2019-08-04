e02f8adcd1848685ed29aae9d782e9f60772e9fe
Guilherme Vieira <guilhermevieiracosta25@gmail.com>
2019-08-04 17:07:28 -0300

    bloqueando o ctrl v

---
+++ aula012/ex012.js
@@ -0,0 +1,20 @@
+var agora = new Date();
+var diaSem = agora.getDay();
+ 
+//console.log(diaSem);
+
+switch(diaSem)
+{
+    case 0:
+        console.log('Domingo')
+        break
+
+
+    case 1:
+        console.log('Segunda')
+        break
+
+    case 6:
+    console.log('Sábado')
+    break    
+}
